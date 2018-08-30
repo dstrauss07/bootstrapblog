54721c3d2edc1ea7c7a01072c10fe95d32d56a73
dstrauss <fmdenver@gmail.com>
2018-08-30 01:05:14 -0400

    playing with the blog

---
+++ js/js.js
@@ -0,0 +1,14 @@
+var subMenu = document.getElementById('submenu'),
+submenuToggle = document.getElementById('submenu-toggle'),
+closeSub = document.getElementsByClassName('close-sub');
+
+
+for(var i=0; i<closeSub.length; i++) {
+    closeSub[i].addEventListener('click', function(){
+        $('subMenu').on('shown.bs.collapse', function(){
+            this.hide.bs.collapse
+        })
+                                             }      
+                                                    }
+
+    )}
